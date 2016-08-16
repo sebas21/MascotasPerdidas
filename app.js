2752047745157a42ecaa1da7b09081e3d0bf6690
sebas21 <sebitis21@hotmail.com>
2016-08-16 00:25:41 -0500

    Se configuro el servidor ( ya abre la pagina index)

---
+++ app.js
@@ -0,0 +1,32 @@
+//dependencias
+
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser  = require("body-parser");
+var methodOverride = require("method-override");
+var path = require('path');
+var app = express();
+var indexRoute = require('./routes/index');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());  
+app.use(methodOverride());
+
+//creacion de una ruta relativa "public"
+app.use(express.static('public'));
+
+//uso de la ruta relativa "public" para utilizar los archivos html de la carpeta "views"
+app.set('views', path.join(__dirname, 'public/views'));
+app.engine("html",require("ejs").renderFile);
+app.set('view engine', 'html');
+
+app.use('/',indexRoute);
+
+//Creacion del puerto 
+app.listen(3000, function() {  
+	console.log("Servidor escuchando en el puerto 3000");
+});
+//exportacion de la variable "app" para poder utilizarse en otros ficheros
+module.exports = app;
+
